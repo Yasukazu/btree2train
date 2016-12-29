57a138449df2d592f288e3452361338290a584e9
Yasukazu <mail@yasukazu.jp>
2016-12-29 19:15:51 +0900

    2 tree frames: float and string

--- src/gui2main.kt
+++ src/gui2main.kt
@@ -9,18 +9,30 @@ import javax.swing.JFrame
 import javax.swing.SwingUtilities
 
 fun main(args:Array<String>){
-    val treeModel = BinarySearchTreeModel<Int>()
+    val treeModel = BinarySearchTreeModel<Float>()
     args.forEach { a ->
-        val i = try { a.toInt() } catch (e: NumberFormatException){
+        val i = try { a.toFloat() } catch (e: NumberFormatException){
             println("$a caused $e")
             null }
         if (i != null){
             treeModel += i
         }
     }
+    val stringTreeModel = BinarySearchTreeModel<String>()
+    args.forEach { i ->
+            stringTreeModel += i
+    }
     SwingUtilities.invokeLater {
-        val intTreeFrame = BinarySearchTreeFrame(treeModel){try{it.toInt()}catch (e:NumberFormatException){null}}
+        val intTreeFrame = BinarySearchTreeFrame(treeModel){try{it.toFloat()}catch (e:NumberFormatException){null}}
         with(intTreeFrame){
+            title = "Real Number Tree"
+            defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+            pack()
+            isVisible = true
+        }
+        val stringTreeFrame = BinarySearchTreeFrame(stringTreeModel){it}
+        with(stringTreeFrame){
+            title = "String Tree"
             defaultCloseOperation = JFrame.EXIT_ON_CLOSE
             pack()
             isVisible = true
