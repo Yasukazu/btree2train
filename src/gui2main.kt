5f8bdb1ab160f53411df1fc5040d0ee72993973a
Yasukazu <mail@yasukazu.jp>
2017-01-07 18:26:54 +0900

    remove serialize button each frame for each loop get arguments when type::class

--- src/gui2main.kt
+++ src/gui2main.kt
@@ -6,6 +6,7 @@
 import jp.yasukazu.kotlin.train.BinarySearchTreeModel
 import jp.yasukazu.kotlin.train.BinarySearchTreeFrame
 import jp.yasukazu.kotlin.train.KollationKey
+import jp.yasukazu.kotlin.train.NormalizerCollator
 import java.awt.Rectangle
 import java.io.FileOutputStream
 import java.io.ObjectOutputStream
@@ -16,36 +17,54 @@ import java.util.*
 import javax.swing.JButton
 import javax.swing.JFrame
 import javax.swing.SwingUtilities
+import kotlin.reflect.jvm.internal.pcollections.HashPMap
 
 
 fun main(args:Array<String>){
-    val treeModel = BinarySearchTreeModel<Float>()
+    val floatTreeModel = BinarySearchTreeModel<Float>()
+    val stringTreeModel = BinarySearchTreeModel<String>()
+    val kollatorTreeModel = BinarySearchTreeModel<KollationKey>()
+    val normalizerCollator = NormalizerCollator()
+    //data class ComparablePairTreeModel<T:Comparable<T>>(val key: class, val model: BinarySearchTreeModel<T> )
+    val treeModelMap = mapOf(Pair(Float::class, floatTreeModel), Pair(String::class, stringTreeModel), Pair(KollationKey::class, kollatorTreeModel))
     args.forEach { a ->
-        val i = try { a.toFloat() } catch (e: NumberFormatException){
-            println("$a caused $e")
-            null }
-        if (i != null){
-            treeModel += i
+        treeModelMap.forEach {
+            val (k, model) = it
+            when(k){
+                Float::class ->
+                    try {
+                        floatTreeModel.insert(a.toFloat())
+                    } catch (e: NumberFormatException){
+                        println("$a caused $e")
+                    }
+                String::class -> stringTreeModel.insert(a)
+                KollationKey::class -> kollatorTreeModel.insert(normalizerCollator[a])
+            }
         }
     }
-    val stringTreeModel = BinarySearchTreeModel<String>()
-    args.forEach { i ->
-            stringTreeModel += i
-    }
-
-    val kollatorTreeModel = BinarySearchTreeModel<KollationKey>()
+    /*
     fun fromStringToKollationKey(it: String): KollationKey {
         val collator = Collator.getInstance()//Locale.JAPANESE)
         collator.strength = Collator.SECONDARY
         return KollationKey(collator.getCollationKey(Normalizer.normalize((it), Normalizer.Form.NFKC)))
-    }
-    args.forEach {it ->
-        kollatorTreeModel += fromStringToKollationKey(it)
-    }
+    } */
 
     SwingUtilities.invokeLater {
-        val intTreeFrame = BinarySearchTreeFrame(treeModel){it.toFloat()} // catch (e:NumberFormatException){null}}
+        val intTreeFrame = BinarySearchTreeFrame(floatTreeModel){it.toFloat()} // catch (e:NumberFormatException){null}}
         with(intTreeFrame){
+            /*
+ val serializeButton = JButton("Serialize to $filename")
+ serializeButton.addActionListener {
+ val list = kollatorTreeModel.preTraverseList()
+ val filename = "___outputStream___.$$$"
+
+     FileOutputStream(filename).use { output ->
+         ObjectOutputStream(output).use { oo ->
+             oo.writeObject(list)
+         }
+     }
+ }
+ entryPanel.add(serializeButton) */
             title = "Real Number Tree"
             defaultCloseOperation = JFrame.EXIT_ON_CLOSE
             setBounds(0, 0, 400, 600)
@@ -60,19 +79,9 @@ fun main(args:Array<String>){
             pack()
             isVisible = true
         }
-        val kollationKeyFrame = BinarySearchTreeFrame(kollatorTreeModel, ::fromStringToKollationKey)
+        val kollationKeyFrame = BinarySearchTreeFrame(kollatorTreeModel) { normalizerCollator[it] }//::fromStringToKollationKey)
         with(kollationKeyFrame){
-            val list = kollatorTreeModel.preTraverseList()
-            val filename = "___outputStream___.$$$"
-            val serializeButton = JButton("Serialize to $filename")
-            serializeButton.addActionListener {
-                FileOutputStream(filename).use { output ->
-                    ObjectOutputStream(output).use { oo ->
-                        oo.writeObject(list)
-                    }
-                }
-            }
-            entryPanel.add(serializeButton)
+
             title = "Collation String Tree"
             defaultCloseOperation = JFrame.EXIT_ON_CLOSE
             setBounds(100, 100, 400, 600)
