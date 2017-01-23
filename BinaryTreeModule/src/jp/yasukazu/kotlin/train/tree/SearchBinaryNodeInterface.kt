62d7d5c7c830a97f2d9ce8eb83cc41ed968e7c17
Yasukazu <mail@yasukazu.jp>
2017-01-24 08:54:52 +0900

    fun add is implemented in the interface

--- BinaryTreeModule/src/jp/yasukazu/kotlin/train/tree/SearchBinaryNodeInterface.kt
+++ BinaryTreeModule/src/jp/yasukazu/kotlin/train/tree/SearchBinaryNodeInterface.kt
@@ -5,19 +5,35 @@ package jp.yasukazu.kotlin.train.tree
  * Created by Yasukazu on 2017/01/22.
  */
 
-enum class InsertedPos { LEFT, NEW, RIGHT, NONE }
 
-/**
- * Node Interface is only about self, right and left
- * includes recursive properties/functions
- */
-class _FoundException(val found: Boolean) : Exception()//, val node: SearchBinaryNodeInterface<T>?, val parent: SearchBinaryNodeInterface<T>?): Exception()
 interface SearchBinaryNodeInterface<T: Comparable<T>>{
     var key: T
     var left: SearchBinaryNodeInterface<T>?
     var right: SearchBinaryNodeInterface<T>?
-    fun add(item: T, callback: ((InsertedPos)->Unit)?)
-    tailrec fun _find(item: T, node: SearchBinaryNodeInterface<T>?, parent: SearchBinaryNodeInterface<T>?, callback: ((SearchBinaryNodeInterface<T>?, SearchBinaryNodeInterface<T>?)->Unit)?){
+    fun new(key: T): SearchBinaryNodeInterface<T> // psudo constructor
+
+
+
+    fun add(item: T, callback: ((InsertedPos)->Unit)?=null){
+        //try {
+        val (found, node, parent) = findNode(item)
+        if (found)
+            throw InsertFailException()
+        assert(node == null) {"Node must be null!"}
+        assert(parent != null) {"Parent must be non-null!"}
+        if (item < key) {
+           parent!!.left = new(item)
+           if (callback != null)
+               callback(InsertedPos.LEFT)
+        }
+        else {
+           parent!!.right = new(item)
+           if (callback != null)
+               callback(InsertedPos.RIGHT)
+        }
+    }
+
+    tailrec fun _find(item: T, node: SearchBinaryNodeInterface<T>?, parent: SearchBinaryNodeInterface<T>?, callback: ((SearchBinaryNodeInterface<T>?, SearchBinaryNodeInterface<T>?)->Unit)?=null){
         if (node == null) {
             if (callback != null)
                 callback(node, parent)
@@ -34,29 +50,28 @@ interface SearchBinaryNodeInterface<T: Comparable<T>>{
             }
         }
     }
-    fun findNode(item: T): Pair<SearchBinaryNodeInterface<T>?, SearchBinaryNodeInterface<T>?> {
+    fun findNode(item: T): Triple<Boolean, SearchBinaryNodeInterface<T>?, SearchBinaryNodeInterface<T>?> {
         var node: SearchBinaryNodeInterface<T>? = null
         var parent: SearchBinaryNodeInterface<T>? = null
+        var found = false
         try {
             _find(item, this, null) { n, p -> node = n
                 parent = p}
         } catch (ex: _FoundException){
-            if (ex.found)
-                return Pair(node, parent)
+            found = ex.found
         }
-        return Pair(null, null)
+        return Triple(found, node, parent)
     }
     operator fun contains(item: T): Boolean // at most 3 members
     fun isLeaf() = left == null && right == null
     fun childCount() = (if (left != null) 1 else 0) + (if(right != null) 1 else 0)
     fun memberCount() = childCount() + 1
     fun childStatus() = (if (left != null) 1 else 0) or (if(right != null) 2 else 0)
+    val childrenStatus: Int get() = (if (left != null) 1 else 0) + (if (right != null) 2 else 0)
     val min: T
     val max: T
     val total: Int get() { return count()}
-    /**
-     * recursive count self and iterCount
-     */
+
     fun count(): Int {
         var n = 0
         traverse { ++n }
@@ -67,6 +82,13 @@ interface SearchBinaryNodeInterface<T: Comparable<T>>{
         _traverse(this, callback)
     }
 
-    fun _traverse(node: SearchBinaryNodeInterface<T>?, callback : ((T) -> Unit)?)
+    fun _traverse(node: SearchBinaryNodeInterface<T>?, callback : ((T) -> Unit)?){
+        if (node == null)
+            return
+        if (callback != null)
+            callback(node.key)
+        _traverse(node.left, callback)
+        _traverse(node.right, callback)
+    }
 
 }
